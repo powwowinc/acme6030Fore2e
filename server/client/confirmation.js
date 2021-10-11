b3be4b73a3798f566c559ea3b2df767447b2c11a
SmartuxQA <91277291+SmartuxQA@users.noreply.github.com>
2021-10-11 12:19:25 +0530

    Update confirmation.js
    done correction in url

--- server/client/confirmation.js
+++ server/client/confirmation.js
@@ -1,3 +1,3 @@
 exports.done = async (session, models, vars) => {
-    await session.transform.acmetransferrequests.url('http://ec2-34-222-124-149.us-west-2.compute.amazonaws.com/assettransfer/login.html');
+    await session.transform.acmetransferrequests.url('http://ec2-34-222-124-149.us-west-2.compute.amazonaws.com/assettransfer/asset-transfer.html');
 };
